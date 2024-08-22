bd25c27e3d37df4a2e7e90a5a5bca2c8db01af76
rakesh_pal <rakesh.pal@mobulous.com>
2024-08-22 10:19:37 +0530

    api implementaion

--- client/src/redux/thunk/vendorServices.js
+++ client/src/redux/thunk/vendorServices.js
@@ -1,69 +1,79 @@
 // authServices.js
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios  from '../../utils/Axios';
-import { BASE_URL } from '../../utils/Urls';
-
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../utils/Axios";
+import { BASE_URL } from "../../utils/Urls";
 
 export const createVendorServices = createAsyncThunk(
-    "createVendorServices",
-    async (payload) => {
-      try {
-        let url = `${BASE_URL}inv/createcategory`;
-        const res = await axios.post(url,payload);
-        return res.data;
-      } catch (error) {
-        //   console.log(error);
-        // handleError(error);
-        throw error;
-      }
+  "createVendorServices",
+  async (payload) => {
+    try {
+      let url = `${BASE_URL}inv/createcategory`;
+      const res = await axios.post(url, payload);
+      return res.data;
+    } catch (error) {
+      //   console.log(error);
+      // handleError(error);
+      throw error;
     }
-  );
+  }
+);
 
-
-  export const updateVendorServices = createAsyncThunk(
-    "updateVendorServices",
-    async (payload) => {
-      try {
-        let url = `${BASE_URL}inv/updatecategory`;
-        const res = await axios.post(url,payload);
-        return res.data;
-      } catch (error) {
-        //   console.log(error);
-        // handleError(error);
-        throw error;
-      }
+export const updateVendorServices = createAsyncThunk(
+  "updateVendorServices",
+  async (payload) => {
+    try {
+      let url = `${BASE_URL}inv/updatecategory`;
+      const res = await axios.post(url, payload);
+      return res.data;
+    } catch (error) {
+      //   console.log(error);
+      // handleError(error);
+      throw error;
     }
-  );
-  
+  }
+);
 
-
-  export const getVendorListServices = createAsyncThunk(
-    "getVendorListServices",
-    async (payload) => {
-      try {
-        let url = `${BASE_URL}inv/getcategory`;
-        const res = await axios.post(url,payload);
-        return res.data;
-      } catch (error) {
-        //   console.log(error);
-        // handleError(error);
-        throw error;
-      }
+export const getVendorListServices = createAsyncThunk(
+  "getVendorListServices",
+  async (payload) => {
+    try {
+      let url = `${BASE_URL}inv/getcategory`;
+      const res = await axios.post(url, payload);
+      return res.data;
+    } catch (error) {
+      //   console.log(error);
+      // handleError(error);
+      throw error;
     }
-  );
+  }
+);
 
-
-  export const getSearchVendorListServices = createAsyncThunk(
-    "getSearchVendorListServices",
-    async (payload) => {
-      try {
-        let url = `${BASE_URL}inv/getcategory`;
-        const res = await axios.post(url,payload);
-        return res.data;
-      } catch (error) {
-        //   console.log(error);
-        // handleError(error);
-        throw error;
-      }
+export const getSearchVendorListServices = createAsyncThunk(
+  "getSearchVendorListServices",
+  async (payload) => {
+    try {
+      let url = `${BASE_URL}inv/getcategory`;
+      const res = await axios.post(url, payload);
+      return res.data;
+    } catch (error) {
+      //   console.log(error);
+      // handleError(error);
+      throw error;
     }
-  );
+  }
+);
+
+export const getAllVendorsServices = createAsyncThunk(
+  "getAllVendorsServices",
+  async () => {
+    try {
+      let url = `${BASE_URL}inv/vendors`;
+      const res = await axios.get(url);
+      return res.data;
+    } catch (error) {
+      //   console.log(error);
+      // handleError(error);
+      throw error;
+    }
+  }
+);
