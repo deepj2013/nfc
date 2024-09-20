f333c38cba3a87641399d3edaf4dba70c7f0a748
deepj2013 <deepj2013@gmail.com>
2024-09-20 20:57:49 +0530

    just fix

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,65 +1,57 @@
-import React from 'react'
-import Chart from '../../components/common/Chart'
-import CircleChart from '../../components/common/CircleChart'
-import DeatilCard from '../../components/dashboard/DeatilCard'
-import Table from '../../components/common/Table'
+import React from "react";
+import Chart from "../../components/common/Chart";
+import CircleChart from "../../components/common/CircleChart";
+import DeatilCard from "../../components/dashboard/DeatilCard";
+import Table from "../../components/common/Table";
+
 
 function Dashboard() {
-    return (
-        <div className='h-screen overflow-scroll '>
-
-            <div className='flex justify-between bg-white mb-6 items-center relative h-40 p-4 rounded-2xl shadow'>
-                <div>
-                <p className='text-black font-semibold h28'>Good Morning, <span className='text-theme'>Daniel Bruk </span></p>
-                <h5 class=" text-base  font-medium  text-grayText  mt-2">Have a nice day at work
-
-</h5>
-                </div>
-              
-
-                <div className='absolute right-0 bottom-0'>
-                    <img src={'https://preclinic.dreamstechnologies.com/html/template/assets/img/morning-img-01.png'}/>
-                </div>
-            </div>
-
-
-            <div className='flex justify-between'>
-               
-                <DeatilCard
-                    tittle={'Appointments'}
-                    icon={'https://preclinic.dreamstechnologies.com/html/template/assets/img/icons/calendar.svg'}
-                />
-                <DeatilCard
-
-                />
-                <DeatilCard />
-                <DeatilCard />
-
-
-            </div>
-
-            <div className='flex  mt-10'>
-                <div className='bg-white w-[100%]  p-6'>
-                    <Chart />
-
-                </div>
-
-            </div>
-
-
-            <div>
-
-
-
-
-            </div>
-
-
-<div>
-    <Table/>
-</div>
+  return (
+    <div className="h-screen overflow-scroll ">
+      <div className="flex justify-between bg-white mb-6 items-center relative h-40 p-4 rounded-2xl shadow">
+        <div>
+          <p className="text-black font-semibold h28">
+            Good Morning, <span className="text-theme"> </span>
+          </p>
+          <h5 class=" text-base  font-medium  text-grayText  mt-2">
+            Have a nice day at work
+          </h5>
         </div>
-    )
+
+        <div className="absolute right-0 bottom-0">
+          <img
+            src={
+              "https://preclinic.dreamstechnologies.com/html/template/assets/img/morning-img-01.png"
+            }
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-between">
+        <DeatilCard
+          tittle={"Appointments"}
+          icon={
+            "https://preclinic.dreamstechnologies.com/html/template/assets/img/icons/calendar.svg"
+          }
+        />
+        <DeatilCard />
+        <DeatilCard />
+        <DeatilCard />
+      </div>
+
+      <div className="flex  mt-10">
+        <div className="bg-white w-[100%]  p-6">
+          <Chart />
+        </div>
+      </div>
+
+      <div></div>
+
+      <div>
+        <Table />
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
