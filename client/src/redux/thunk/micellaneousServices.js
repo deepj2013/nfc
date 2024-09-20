75bbf4bae6e5841777581229c5089f1521be5cbb
Sachin <sac123sh@gmail.com>
2024-09-20 23:17:17 +0530

    fix

--- client/src/redux/thunk/micellaneousServices.js
+++ client/src/redux/thunk/micellaneousServices.js
@@ -3,19 +3,22 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../../utils/Axios";
 import { BASE_URL, BASE_URL_SUPER_ADMIN } from "../../utils/Urls";
 import { handleError } from "../../utils/ErrorHandler";
+import { getStorageValue } from "../../services/LocalStorageServices";
+
+
 
 export const getAllMenuServices = createAsyncThunk(
-  "getAllMenuServices",
-  async () => {
-    try {
-      let url = `${BASE_URL_SUPER_ADMIN}getallmenu`;
-      const res = await axios.get(url);
-      return res.data;
-    } catch (error) {
-        handleError(error)
-      throw error;
+    "getAllMenuServices",
+    async () => {
+        try {
+            let url = `${BASE_URL_SUPER_ADMIN}getallmenu`;
+            const res = await axios.get(url);
+            return res.data;
+        } catch (error) {
+            handleError(error)
+            throw error;
+        }
     }
-  }
 );
 
 
@@ -23,13 +26,15 @@ export const getAllMenuServices = createAsyncThunk(
 export const getMenuByRoleServices = createAsyncThunk(
     "getMenuByRoleServices",
     async (payload) => {
-      try {
-        let url = `${BASE_URL_SUPER_ADMIN}getmenubyrole`;
-        const res = await axios.post(url,payload);
-        return res.data;
-      } catch (error) {
-          handleError(error)
-        throw error;
-      }
+        try {
+            let userDetails = getStorageValue('userDetails')
+            console.log(userDetails.role_id,'userDetails');
+            let url = `${userDetails?.role_id == 1 ? BASE_URL_SUPER_ADMIN : BASE_URL}getmenubyrole`;
+            const res = await axios.post(url, payload);
+            return res.data;
+        } catch (error) {
+            handleError(error)
+            throw error;
+        }
     }
-  );
+);
