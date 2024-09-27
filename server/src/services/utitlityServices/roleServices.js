5792589c646cc6001caf35c3a0b30140a199fcc0
deepj2013 <deepj2013@gmail.com>
2024-09-27 18:51:57 +0530

    fix roleId issue

--- server/src/services/utitlityServices/roleServices.js
+++ server/src/services/utitlityServices/roleServices.js
@@ -1,14 +1,19 @@
 import Role from '../../models/roleModel.js';
 // import { getRoleMenus } from '../../helpers/utilityHelper/roleHelper.js'
-
 export const createRoleService = async (roleData) => {
-        const lastRole= await Role.findOne().sort({ roleId: -1 }).exec();
-        let role_id = lastRole ? lastRole.role_id + 1 : 1;
-        roleData.role_id = role_id;
+    // Find the last role by sorting role_id in descending order
+    const lastRole = await Role.findOne().sort({ role_id: -1 }).exec();
     
+    // If a role exists, increment role_id by 1; otherwise, start from 1
+    let role_id = lastRole ? lastRole.role_id + 1 : 1;
+    roleData.role_id = role_id;
+
+    // Create and save the new role
     const role = new Role(roleData);
     return await role.save();
 };
+
+
 
 export const getRoleService = async () => {
     const pipeline = [
