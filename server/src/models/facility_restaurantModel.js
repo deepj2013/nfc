9ee278145dc8af1d27aac28343c29dbe9eb66105
deepj2013 <deepj2013@gmail.com>
2024-09-27 13:51:12 +0530

    restaurant

---
+++ server/src/models/facility_restaurantModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const restaurantSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    type: { type: String, enum: ['Own', 'Partnership', 'Third-Party'], required: true },
+    commissionRate: { type: Number, default: 0 },  // Used for third-party or partnership restaurants
+    tables: [{
+        tableNumber: { type: Number, required: true },
+        capacity: { type: Number, required: true },
+        currentStatus: { type: String, enum: ['Available', 'Occupied'], default: 'Available' }
+    }]
+}, { timestamps: true });
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+export default Restaurant;
