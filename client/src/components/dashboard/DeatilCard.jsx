64fa6da603a8351065bcefe0297c853538665e39
deepj2013 <deepj2013@gmail.com>
2024-10-08 22:22:41 +0530

    favicon and logo insert

--- client/src/components/dashboard/DeatilCard.jsx
+++ client/src/components/dashboard/DeatilCard.jsx
@@ -7,9 +7,9 @@ function DeatilCard({tittle,value,image}) {
           <img className='h-8 w-8' src='https://preclinic.dreamstechnologies.com/html/template/assets/img/icons/calendar.svg'/>
         </div>
           <a href="#">
-            <h5 class="mb-2 h24  font-medium tracking-tight text-gray-900 mt-2">Appointments</h5>
+            <h5 class="mb-2 h24  font-medium tracking-tight text-gray-900 mt-2">Room/ hall Request </h5>
           </a>
-          <p class="mb-3  text-theme font-semibold text-3xl">250</p>
+          <p class="mb-3  text-theme font-semibold text-3xl">25</p>
      <div className='text-gray-500 font-light'>
     <span className='text-green-500 text-sm'> 40%  </span>vs last month
 
