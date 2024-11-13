85f3933ce9436d4f51dd756913ce821dfdcd003f
deepj2013 <deepj2013@gmail.com>
2024-11-13 14:42:07 +0530

    build 1.0.7

--- client/src/utils/Urls.js
+++ client/src/utils/Urls.js
@@ -9,5 +9,6 @@ export const BASE_URL = "https://api.k2krafts.in/api/user/";
 export const BASE_URL_FILEUPLOAD = "https://api.k2krafts.in/api/";
 
 export const BASE_URL_SUPER_ADMIN = "https://api.k2krafts.in/api/admin/";
+export const URL = "https://api.k2krafts.in/api/";
 
 // export const URL = "http://localhost:5100/api/";
