0f1692bc85929ed5199e24a8323be513cf06bba6
root <root@br-sfs.in>
2025-01-21 15:57:08 +0000

    add some image

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 const configObj = {};
 
-// let conn = "mongodb://localhost:27017/nfc-local";
-let conn = "mongodb+srv://deepmca2014:deepmca2014@cluster0.zrjpwj3.mongodb.net/nfc"
+ let conn = "mongodb://localhost:27017/nfc-local";
+//let conn = "mongodb+srv://deepmca2014:deepmca2014@cluster0.zrjpwj3.mongodb.net/nfc"
 
 
 // if (process.env.ENV == "prod") {
