9a67d49306b5bc96a65c29818e32d60e16763dd7
deepj2013 <deepj2013@gmail.com>
2025-04-09 19:14:15 +0530

    fix

--- client/src/utils/Urls.js
+++ client/src/utils/Urls.js
@@ -1,18 +1,18 @@
-export const BASE = "http://localhost:5100/api/"
-export const BASE_URL = "http://localhost:5100/api/user/";
+// export const BASE = "http://localhost:5100/api/"
+// export const BASE_URL = "http://localhost:5100/api/user/";
 
-export const BASE_URL_FILEUPLOAD = "http://localhost:5100/api/";
+// export const BASE_URL_FILEUPLOAD = "http://localhost:5100/api/";
 
-export const BASE_URL_SUPER_ADMIN = "http://localhost:5100/api/admin/";
-export const BASE_URL_FACILITY = "http://localhost:5100/api/facilities/"
-export const URL = "http://localhost:5100/api/";
+// export const BASE_URL_SUPER_ADMIN = "http://localhost:5100/api/admin/";
+// export const BASE_URL_FACILITY = "http://localhost:5100/api/facilities/"
+// export const URL = "http://localhost:5100/api/";
 
-// export const BASE = "https://api.newfriendsclubdelhi.in/api/"
-// export const BASE_URL = "https://api.newfriendsclubdelhi.in/api/user/";
+export const BASE = "https://api.newfriendsclubdelhi.in/api/"
+export const BASE_URL = "https://api.newfriendsclubdelhi.in/api/user/";
 
-// export const BASE_URL_FILEUPLOAD = "https://api.newfriendsclubdelhi.in/api/";
-// export const BASE_URL_FACILITY = "https://api.newfriendsclubdelhi.in/api/facilities/"
+export const BASE_URL_FILEUPLOAD = "https://api.newfriendsclubdelhi.in/api/";
+export const BASE_URL_FACILITY = "https://api.newfriendsclubdelhi.in/api/facilities/"
 
-// export const BASE_URL_SUPER_ADMIN = "https://api.newfriendsclubdelhi.in/api/admin/";
-// export const URL = "https://api.newfriendsclubdelhi.in/api/";
+export const BASE_URL_SUPER_ADMIN = "https://api.newfriendsclubdelhi.in/api/admin/";
+export const URL = "https://api.newfriendsclubdelhi.in/api/";
 
